1d0c2b3f1d5acb51e96b0545cca328adecfef688
Maxi <maximilian.lincks@gmx.net>
2023-04-19 17:52:59 +0200

    added implementation of hit-miss operator

--- src/morphologic_operators/core.clj
+++ src/morphologic_operators/core.clj
@@ -48,6 +48,29 @@
 
 (defmethod invert PersistentVector [val]
     (vec (map invert val)))
+(defn morphologic-base
+    [definition image mask]
+    (let [{:keys [from to condition]} definition
+          image-max-x (count (nth image 0))
+          image-max-y (count image)
+          mask-max-x (count (nth mask 0))
+          mask-max-y (count mask)
+          mask-center-x (/ (- mask-max-x 1) 2)
+          mask-center-y (/ (- mask-max-y 1) 2)]
+        (to-matrix image-max-x
+                   (for [y (range image-max-y)
+                         x (range image-max-x)
+                         :let [y'x' (for [y' (range mask-max-y)
+                                          x' (range mask-max-x)]
+                                        [y' x'])
+                               ;coordinates in the mask with 1
+                               y'x'-ones (filter #(= 1 (get-at mask %)) y'x')
+                               ;transform to offsets in image
+                               y'x'-offset (map #(map - % [mask-center-y mask-center-x]) y'x'-ones)
+                               y'x'-image (map #(map + % [y x]) y'x'-offset)
+                               ;look only at points in the image with valid coordinates
+                               y'x'-valid (filter #(is-valid? image-max-y image-max-x %) y'x'-image)]]
+                       (if (condition image y x y'x'-valid) to from)))))
 (defn errosion
     "1 -> 0
     image and mask are both matrices containing only 0 and 1
@@ -56,26 +79,10 @@
     odd length mask
     odd height mask"
     ([image mask]
-     (let [image-max-x (count (nth image 0))
-           image-max-y (count image)
-           mask-max-x (count (nth mask 0))
-           mask-max-y (count mask)
-           mask-center-x (/ (- mask-max-x 1) 2)
-           mask-center-y (/ (- mask-max-y 1) 2)]
-         (to-matrix image-max-x
-                    (for [y (range image-max-y)
-                          x (range image-max-x)
-                          :let [y'x' (for [y' (range mask-max-y)
-                                           x' (range mask-max-x)]
-                                         [y' x'])
-                                ;coordinates in the mask with 1
-                                y'x'-ones (filter #(= 1 (get-at mask %)) y'x')
-                                ;transform to offsets in image
-                                y'x'-offset (map #(map - % [mask-center-y mask-center-x]) y'x'-ones)
-                                y'x'-image (map #(map + % [y x]) y'x'-offset)
-                                ;look only at points in the image with valid coordinates
-                                y'x'-valid (filter #(is-valid? image-max-y image-max-x %) y'x'-image)]]
-                        (if (or (= 0 (get-at image y x)) (errosion-check image y'x'-valid)) 0 1)))))
+     (morphologic-base {:from 1 :to 0 :condition (fn [image y x y'x'-valid]
+                                                     (or (= 0 (get-at image y x))
+                                                         (errosion-check image y'x'-valid)))}
+                       image mask))
     ([image] (errosion image (ones 3 3))))
 
 (defn diletation-check [image yx] "checks if the image has a 1 at any given coordinate"
@@ -89,26 +96,10 @@
     odd length mask
     odd height mask"
     ([image mask]
-     (let [image-max-x (count (nth image 0))
-           image-max-y (count image)
-           mask-max-x (count (nth mask 0))
-           mask-max-y (count mask)
-           mask-center-x (/ (- mask-max-x 1) 2)
-           mask-center-y (/ (- mask-max-y 1) 2)]
-         (to-matrix image-max-x
-                    (for [y (range image-max-y)
-                          x (range image-max-x)
-                          :let [y'x' (for [y' (range mask-max-y)
-                                           x' (range mask-max-x)]
-                                         [y' x'])
-                                ;coordinates in the mask with 1
-                                y'x'-ones (filter #(= 1 (get-at mask %)) y'x')
-                                ;transform to offsets in image
-                                y'x'-offset (map #(map - % [mask-center-y mask-center-x]) y'x'-ones)
-                                y'x'-image (map #(map + % [y x]) y'x'-offset)
-                                ;look only at points in the image with valid coordinates
-                                y'x'-valid (filter #(is-valid? image-max-y image-max-x %) y'x'-image)]]
-                        (if (or (= 1 (get-at image y x)) (diletation-check image y'x'-valid)) 1 0)))))
+     (morphologic-base {:from 0 :to 1 :condition (fn [image y x y'x'-valid]
+                                                     (or (= 1 (get-at image y x))
+                                                         (diletation-check image y'x'-valid)))}
+                       image mask))
     ([image] (diletation image (ones 3 3))))
 
 (defn opening "removes clusters and noise"
@@ -124,3 +115,20 @@
          (diletation mask)
          (errosion mask)))
     ([image] (closing image (ones 3 3))))
+
+(defn intersection [matrix1 matrix2]
+    (vectorize (map #(map bit-and %1 %2) matrix1 matrix2 )))
+
+(defn union [matrix1 matrix2]
+    (vectorize (map #(map bit-or %1 %2) matrix1 matrix2 )))
+
+(defn hit-miss
+    "(intersection mask1 mask2) should be all zeros,
+    if the masks don't have the same size they are expanded with zero padding"
+    [image mask1 mask2]
+    (intersection (diletation image mask1)
+           (invert (diletation image mask2))))
+(def hit-miss-m1 [[1 1 1]])
+(def hit-miss-m2 [[1 1 1 1 1]
+                  [1 0 0 0 1]
+                  [1 1 1 1 1]])
